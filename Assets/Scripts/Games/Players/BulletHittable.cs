b15c6d426a772671fb22e19394a29c3e7b2bf547
Shoma SATO <noir.neo.04@gmail.com>
2018-08-11 21:26:16 +0900

    Hit and explode

--- Assets/Scripts/Games/Players/BulletHittable.cs
+++ Assets/Scripts/Games/Players/BulletHittable.cs
@@ -5,7 +5,7 @@ using Zenject;
 
 namespace Games.Players
 {
-    public class BulletHittable : MonoBehaviour, IFireworksEnterHandler
+    public class BulletHittable : MonoBehaviour, IFireworksEnterHandler, IFireworksExitHandler
     {
         [Inject]
         StateStore stateStore;
@@ -13,6 +13,11 @@ namespace Games.Players
         bool IFireworksEnterHandler.IFireworksEnter()
         {
             stateStore.State.Value = State.Dead;
+            return true;
+        }
+
+        public bool IFireworksExit()
+        {
             return true;
         }
     }
