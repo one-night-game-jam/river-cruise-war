2f501069bd135fbd5958d7c3086f230837310b44
Shota HAMADA <maku693@outlook.com>
2018-08-11 17:20:46 +0900

    Add BulletTime

---
+++ Assets/Scripts/Games/Players/BulletTime.cs
@@ -0,0 +1,37 @@
+using UniRx;
+using UnityEngine;
+using Fireworks;
+
+namespace Players
+{
+    public class BulletTime : MonoBehaviour, IFireworksEnterHandler, IFireworksExitHandler
+    {
+        [SerializeField] private float slowMotionTimeScale = 0.1f;
+        [SerializeField] private float normalTimeScale = 1;
+        [SerializeField] private bool isSlowMotion = false;
+
+        public bool IFireworksEnter()
+        {
+            isSlowMotion = true;
+            return false;
+        }
+
+        public bool IFireworksExit()
+        {
+            isSlowMotion = false;
+            return false;
+        }
+
+        private void Update()
+        {
+            if (isSlowMotion)
+            {
+                Time.timeScale = slowMotionTimeScale;
+            }
+            else
+            {
+                Time.timeScale = normalTimeScale;
+            }
+        }
+    }
+}
