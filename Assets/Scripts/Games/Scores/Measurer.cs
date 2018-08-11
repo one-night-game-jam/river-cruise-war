5e911224126925bdec350cf14ebd67adf5627aa2
Shoma SATO <noir.neo.04@gmail.com>
2018-08-11 20:04:18 +0900

    Update score

---
+++ Assets/Scripts/Games/Scores/Measurer.cs
@@ -0,0 +1,34 @@
+﻿using Stores;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using Zenject;
+
+namespace Games.Scores
+{
+    public class Measurer : MonoBehaviour
+    {
+        [SerializeField]
+        float TimeDistanceScale;
+
+        [Inject]
+        StateStore stateStore;
+        [Inject]
+        ScoreStore scoreStore;
+
+        void Start()
+        {
+            this.UpdateAsObservable()
+                .WithLatestFrom(stateStore.State, (_, state) => state)
+                .Where(s => s == State.Playing)
+                .Subscribe(_ => UpdateScore())
+                .AddTo(this);
+        }
+
+        void UpdateScore()
+        {
+            scoreStore.CruisedDistance.Value += Time.deltaTime * TimeDistanceScale;
+        }
+    }
+}
