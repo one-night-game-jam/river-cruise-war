931ddab1f96bf461451d2338198ce52de593cdf4
Shoma SATO <noir.neo.04@gmail.com>
2018-08-11 14:31:35 +0900

    Tweak scene loader

--- Assets/Scripts/Scenes/SceneLoader.cs
+++ Assets/Scripts/Scenes/SceneLoader.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,12 +11,15 @@ namespace Scenes
 
         private void Start()
         {
-            LoadScenesAdditive();
+            var loaded = Enumerable.Range(0, SceneManager.sceneCount)
+                .Select(SceneManager.GetSceneAt)
+                .Select(s => s.name).ToList();
+            LoadScenesAdditive(_sceneNames.Except(loaded));
         }
 
-        private void LoadScenesAdditive()
+        private static void LoadScenesAdditive(IEnumerable<string> sceneNames)
         {
-            foreach (var name in _sceneNames)
+            foreach (var name in sceneNames)
             {
                 SceneManager.LoadScene(name, LoadSceneMode.Additive);
             }
